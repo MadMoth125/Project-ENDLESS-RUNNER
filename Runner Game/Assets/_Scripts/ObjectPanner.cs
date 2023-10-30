ef3283b9186f09c92a5abe80fd359649dc076140
MadMoth125 <madmoth125@gmail.com>
2023-10-30 15:29:02 -0500

    added base project, grid mats, and basic scene

---
+++ Runner Game/Assets/_Scripts/ObjectPanner.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPanner : MonoBehaviour
+{
+	private void OnEnable()
+	{
+		
+	}
+
+	private void OnDisable()
+	{
+		
+	}
+
+	private void Start()
+	{
+		
+	}
+
+	private void Update()
+	{
+		
+	}
+}
